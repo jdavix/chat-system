e4655f664be1acae37e63dd6d705d19ea8ae574a
Jose Pedraza <pedrazaj_24@hotmail.com>
2019-12-09 12:43:08 -0500

    max number of group members validation

--- backend-services/src/models/groupChat.js
+++ backend-services/src/models/groupChat.js
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose';
 
+const MAX_INVITES = 10
+
 const GroupChatSchema = new mongoose.Schema({
   chat_type: {
     type: String,
@@ -13,8 +15,11 @@ const GroupChatSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
   }],
-  // TODO: invitations objects.
-  invitations: [String],
+  invitations: {
+    type: Array,
+    required: true,
+    validate: [(group) => (group.length <= MAX_INVITES), `More than ${MAX_INVITES} users in a chat is not allowed`],
+  },
 },
 {
   timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' },
